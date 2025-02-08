81397aaef43616722b85712a4f3f06211fd7adbb
RAJSHEKHAR DAS <rajshekhardas85@gmail.com>
2025-02-08 23:26:00 +0530

    initialized express

---
+++ app.js
@@ -0,0 +1,35 @@
+const express = require("express");
+const socket= require("socket.io");
+
+const { Chess }=require ("chess.js");
+const path = require("path");
+
+const app = express();
+const http = require("http");
+
+
+const server = http.createServer(app);
+                                           //http ka ek server banake link to express..and socket will run that server
+const io=socket(server);
+
+const chess=new Chess();
+
+let players={};
+let currentPlayer="W"
+
+app.set("view engine", "ejs"); //to use ejs
+
+app.use(express.static(path.join(__dirname,"public")));  //to use static files like images,vanillajs,fonts,videos etc
+
+app.get("/", (req, res) => {
+    res.render("index");
+    });
+
+server.listen(3000, () => {
+    console.log("Server is running on port 3000");
+});
+
+
+
+
+
