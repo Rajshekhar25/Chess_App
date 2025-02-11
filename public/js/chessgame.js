3d0a65dd6b548aab1365d5c0a0dc8a85d84b349e
RAJSHEKHAR DAS <rajshekhardas85@gmail.com>
2025-02-12 02:50:45 +0530

    backend

--- public/js/chessgame.js
+++ public/js/chessgame.js
@@ -1,4 +1,43 @@
 //basic socket.io setup
 
- const socket=io();
+ const socket= io();
+ const chess = new Chess();
+ const boardElement = document.querySelector(".chessboard");
+
+ let draggedPiece=null;
+ let sourceSquare=null;
+ let playerRole=null;
+
+ const renderBoard=()=>{
+    const board =chess.board();
+    boardElement.innerHTML="";
+    board.forEach((row,rowindex)=>{
+        row.forEach((square,squareindex)=>{
+            const squareElement=document.createElement("div");
+            squareElement.classsList.add("square", (rowindex + squareindex)%2 ===0? "light" : "dark");
+
+            squareElement.dataset.row=rowindex;
+            squareElement.dataset.col=squareindex;   //should be .square I suppose
+
+            if(square){
+                const pieceElement = document.createElement("div");
+                pieceElement.classList.add("piece", square.color === 'w'? "white" : "black");
+            }
+      
+        
+      
+        });
+        });
+ 
+    };
+
+
+ const handleMove=()=>{};
+
+ const getPieceUnicode=()=>{};
+
+ renderBoard();
+
+
+
 
